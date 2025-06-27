5f7a0987e913857586e5846ef54e6d74c1e92afd
liamflannery <liam.flannery56@gmail.com>
2025-06-27 13:12:43 +1000

    grid movement and art

---
+++ scenes/grid.gd
@@ -0,0 +1,47 @@
+extends GridContainer
+class_name Grid
+class Tile:
+	var tile_object : TextureRect
+	var tile_position : int
+	func _init(this_object : TextureRect, this_position : int) -> void:
+		tile_object = this_object
+		tile_position = this_position
+
+var all_tiles : Array[Tile]
+var total_tiles : int
+var total_columns : int
+func _ready() -> void:
+	for child in get_children():
+		var tile_object = Tile.new(child, child.get_index())
+		all_tiles.append(tile_object)
+	total_tiles = get_child_count()
+	total_columns = columns
+
+	
+func get_north_tile(from_tile : Tile) -> Tile:
+	var north_position = from_tile.tile_position - total_columns
+	if north_position < 0:
+		return null
+	return all_tiles[north_position]
+
+func get_south_tile(from_tile : Tile) -> Tile:
+	var south_position = from_tile.tile_position + total_columns
+	if south_position >= total_tiles:
+		return null
+	return all_tiles[south_position]
+
+func get_east_tile(from_tile : Tile) -> Tile:
+	var current_row = from_tile.tile_position / total_columns
+	var east_position = from_tile.tile_position + 1
+	var east_row = east_position / total_columns
+	if east_row != current_row or east_position >= total_tiles:
+		return null
+	return all_tiles[east_position]
+
+func get_west_tile(from_tile : Tile) -> Tile:
+	var current_row = from_tile.tile_position / total_columns
+	var west_position = from_tile.tile_position - 1
+	var west_row = west_position / total_columns
+	if west_position < 0 or west_row != current_row:
+		return null
+	return all_tiles[west_position]
